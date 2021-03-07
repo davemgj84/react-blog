b02c2760972a3880f80e62cbcfc60217f8371967
David <davemgj@gmail.com>
2021-03-06 23:56:14 -0800

    create BlogList component & Base Styles, install react router

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,19 @@
 import "../styles/Home.scss";
+import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My New Website", body: "lorem ipsum...", author: "David", id: 1 },
+    { title: "Welcome Party!", body: "lorem ipsum...", author: "David", id: 2 },
+    { title: "React Tips", body: "lorem ipsum...", author: "David", id: 3 },
+  ]);
+
+  const handleClick = () => {};
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      <BlogList blogs={blogs} title="All Blogs!" />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,13 @@ import "../styles/Navbar.scss";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The React Blog</h1>
+      <h1>
+        <a href="/">The React Blog</a>
+      </h1>
       <div className="links">
         <a href="/">Home</a>
         <a href="/create">New Blog</a>
+        <a href="/login">Login</a>
       </div>
     </nav>
   );
