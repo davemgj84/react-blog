2e346a406f82166634e87032fccdd7eb244f9c13
David <davemgj@gmail.com>
2021-03-06 23:12:33 -0800

    create Home and Navbar components. create base styles for Navbar/Home/App (due to change).

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import "./styles/App.scss";
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
 };
 
 export default App;
