eeaf0e58d738ba4b1bb95635de7d376fca23e1a5
David <davemgj@gmail.com>
2021-03-07 00:56:53 -0800

    create mock database using JSON server. Create fetch request to GET blogs data. Create Error Handling

---
+++ data/JsonServer.md
@@ -0,0 +1,15 @@
+# To run server locally without installation
+
+- `npx json-server --watch data/db.json --port 8000`
+
+# Endpoints
+
+- `/blogs` - GET - Fetch all blogs.
+
+- `/blogs/:id` - GET - Fetch a single blog.
+
+- `/blogs` - POST - Add a new blog.
+
+- `/blogs/:id` - DELETE - Delete a blog.
+
+### Will use Json Server until I am ready to create a Node Express API, and PostgreSQL Database
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,40 @@
+import { useEffect, useState } from "react";
 import "../styles/Home.scss";
-import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My New Website", body: "lorem ipsum...", author: "David", id: 1 },
-    { title: "Welcome Party!", body: "lorem ipsum...", author: "David", id: 2 },
-    { title: "React Tips", body: "lorem ipsum...", author: "David", id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const handleClick = () => {};
+  useEffect(() => {
+    // *** Using a setTimeout to simulate loading time - will remove once connected to backend
+    setTimeout(() => {
+      // GET request to local server for all blogs
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Sorry, we could not fetch any data...");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" />
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
