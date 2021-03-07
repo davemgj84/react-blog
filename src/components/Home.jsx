f5b2e065d2ad31e446f0993363dc87c05c8e6c4c
David <davemgj@gmail.com>
2021-03-07 01:06:42 -0800

    create custom hook useFetch, to handle fetch requests - creates more  modular code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,11 @@
-import { useEffect, useState } from "react";
 import "../styles/Home.scss";
 import BlogList from "./BlogList";
+import useFetch from "../hooks/useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    // *** Using a setTimeout to simulate loading time - will remove once connected to backend
-    setTimeout(() => {
-      // GET request to local server for all blogs
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Sorry, we could not fetch any data...");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsLoading(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setIsLoading(false);
-          setError(err.message);
-        });
-    }, 1000);
-  }, []);
+  const { data: blogs, isLoading, error } = useFetch(
+    "http://localhost:8000/blogs"
+  );
 
   return (
     <div className="home">
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // *** Using a setTimeout to simulate loading time - will remove once connected to backend
+    setTimeout(() => {
+      // GET request to local server
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Sorry, we could not fetch any data...");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, [url]);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch;
