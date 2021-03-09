91bcba3c8a99cf7e6ff6e29496071acdd7da6277
David <davemgj@gmail.com>
2021-03-08 20:50:49 -0800

    connect/output data to BlogDetails component using the useFetch Hook

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,12 +1,24 @@
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 import "../styles/BlogDetails.scss";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data: blog, isLoading, error } = useFetch(
+    `http://localhost:8000/blogs/${id}`
+  );
 
   return (
     <div className="blog-details">
-      <h2>Blog Details - {id} </h2>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
