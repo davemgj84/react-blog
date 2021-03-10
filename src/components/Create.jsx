6608d22177cd273c9624576ed081ca06c2b53ee2
David <davemgj@gmail.com>
2021-03-09 20:33:08 -0800

    setup Create component forms and styles. Start to create logic to handle submit

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,9 +1,45 @@
+import { useState } from "react";
 import "../styles/Create.scss";
 
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("David");
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const blog = {
+      title,
+      body,
+      author,
+    };
+    console.log(blog);
+  };
+
   return (
     <div className="create">
       <h2>Create a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(evt) => setTitle(evt.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(evt) => setBody(evt.target.value)}
+        ></textarea>
+        <label>Blog Author:</label>
+        <select value={author} onChange={(evt) => setAuthor(evt.target.value)}>
+          <option value="David">David</option>
+          <option value="Kim">Kim</option>
+        </select>
+        <button>Create Blog</button>
+      </form>
     </div>
   );
 };
